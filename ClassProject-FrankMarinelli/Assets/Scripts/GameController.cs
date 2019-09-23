c93d0d2e4330a5c31fcc6fdea218fb9b0b46ee41
FrankM97 <47196970+FrankM97@users.noreply.github.com>
2019-09-23 18:05:37 -0400

    What I have so far
    Current progress on the homework

---
+++ ClassProject-FrankMarinelli/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public float counterTime;
+	public Text counterText;
+
+	private void Update() {
+		counterTime += Time.deltaTime;
+		counterText.text = "Time: " + Mathf.Round(counterTime).ToString();
+	}
+	//code used is from a previous project from a different class.
+}
